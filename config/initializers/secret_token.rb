d24f652dd3f2f6b5749bd191c0ead775e03aaec5
Lukasz Wegrowski <wegrowski.lukasz@gmail.com>
2013-10-13 14:31:07 +0200

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LukaszHomepage::Application.config.secret_key_base = '3f080783c5afd90d0bd29d29c3adf2e52b9dec126f0d2b076d7ecc30a7df977f31b9c7e103bf4a2d9d2de52331840fbeb712b4861fc9d3cde2905a71bfb23730'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LukaszHomepage::Application.config.session_store :cookie_store, key: '_lukasz_homepage_session'
